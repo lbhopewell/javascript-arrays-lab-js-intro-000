b3ea0953f9d05f533eb246438bf6672ff174b2f4
Bruce Hopewell <lbhopewell@outlook.com>
2018-09-12 02:06:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(kittens){
+  kittens.push("kettin");
+  return kittens;
+}
+
+function destructivelyPrependKitten(kittens){
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  
+}
+
+function appendKitten(name){
+  
+}
+
+function prependKitten(name){
+  
+}
+
+function removeLastKitten(){
+  
+}
+
+function removeFirstKitten(){
+  
+}
+
